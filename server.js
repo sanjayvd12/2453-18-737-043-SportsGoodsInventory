c4c494e17da54caa6e71d6925236c0d3abe8573d
sanjayvd12 <67838465+sanjayvd12@users.noreply.github.com>
2021-04-29 23:50:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,82 @@
+const express=require('express')   
+const app=express()   
+const bodyParser=require('body-parser')   
+const MongoClient=require('mongodb').MongoClient   
+var db; 
+var s;
+
+MongoClient.connect('mongodb://localhost:27017/accessories', (err,database) =>{        
+	if(err) return console.log(err)
+	db=database.db('accessories')     
+	app.listen(5000,() =>{
+		console.log('Listening at port number 5000')
+	})
+})
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))  
+app.use(bodyParser.json())   
+app.use(express.static('public'))  
+
+//homepage
+app.get('/', (req,res)=>{                
+	db.collection('sportsGoods').find().toArray( (err,result)=>{     
+		if(err) return console.log(err)     
+	res.render('homepage.ejs', {data:result})      
+	})
+})
+
+//for add
+app.get('/create', (req,res)=>{     //change
+	res.render('add.ejs')            //change
+})
+
+//for update
+app.get('/updatestock', (req,res)=>{			//change
+	res.render('update.ejs')				//change
+})
+
+
+//for delete
+app.get('/deleteproduct', (req,res)=>{		//change
+	res.render('delete.ejs')		//change
+})
+
+//post request code-
+app.post('/AddData', (req,res)=>{			
+	db.collection('sportsGoods').save(req.body, (err,result)=>{			
+		if(err) return console.log(err)
+	res.redirect('/')                        
+	})
+})
+
+//post-update stock 
+app.post('/update', (req,res)=>{			
+		db.collection('sportsGoods').find().toArray((err,result)=>{	   			
+			if(err) 
+				return console.log(err)
+			for(var i=0;i<result.length;i++){
+				if(result[i].pid==req.body.id){
+					s=result[i].stock
+					break
+				}
+			}
+			db.collection('sportsGoods').findOneAndUpdate({pid:req.body.id},{
+				$set:{stock: parseInt(s) +parseInt(req.body.stock)}} , {sort:{_id:-1}},
+				(err,result) =>{
+					if(err)
+						return console.log(err)
+					console.log(req.body.id+'stock has been updated')
+					res.redirect('/')
+				})
+			})	
+		})
+
+//post- delete stock
+app.post('/delete',(req,res)=>{
+	db.collection('sportsGoods').findOneAndDelete({pid:req.body.id}, (err,result)=>{    
+		if(err)
+			return console.log(err)
+		res.redirect('/')
+	})
+})
